9ebd4f99cc8574c629cd150077557a3210a05bd7
Alberto Santini <santini.alberto@gmail.com>
2020-10-10 23:35:24 +0200

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000', 'https://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/')
+def get(neighbourhood: str, sqm: int, rooms: int, state: str, elevator: bool):
+    data = get_data(neighbourhood)
+    model = get_model(data, sqm, rooms)
+    result = get_result(data, model, sqm, rooms, state, elevator)
+
+    return result
+
+
+def get_data(neighbourhood: str):
+    return pd.read_csv(f"data/{neighbourhood}.csv")
+
+
+def get_model(data: pd.DataFrame, sqm: int, rooms: int):
+    d = data[(data.sqm != sqm) & (data.rooms != rooms)]
+    X = d[['sqm', 'rooms', 'state', 'elevator']]
+    y = d[['price']]
+
+    numeric_features = ['sqm', 'rooms']
+    categorical_features = ['state', 'elevator']
+
+    numeric_transformer = Pipeline(steps=[
+        ('scaler', StandardScaler())])
+    categorical_transformer = Pipeline(steps=[
+        ('encoder', OneHotEncoder(handle_unknown='error', drop='first'))])
+    preprocessor = ColumnTransformer(transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+    model = Pipeline(steps=[
+        ('preprocess', preprocessor),
+        ('linreg', LinearRegression())])
+
+    model.fit(X, y)
+
+    training_preds = model.predict(X).flatten()
+    real_labels = y.values.flatten()
+    stddev = np.sqrt(sum((training_preds - real_labels) ** 2) / (len(y) - 2))
+
+    return dict(pipeline=model, stddev=stddev)
+
+
+def get_prediction(model: dict, sqm: int, rooms: int, state: str, elevator: bool):
+    pred = model['pipeline'].predict(pd.DataFrame(dict(
+        sqm=[sqm], rooms=[rooms], state=[state], elevator=[elevator])))
+    pred = pred.flatten()[0] * .95
+
+    return dict(
+        cost=roundk(pred),
+        lower_bound=roundk(pred - 0.25 * model['stddev']),
+        upper_bound=roundk(pred + 0.25 * model['stddev'])
+    )
+
+
+def get_comparative(data: pd.DataFrame, sqm: int, rooms: int, state: str, elevator: bool):
+    comparative = dict()
+
+    d = data[(data.sqm >= .95 * sqm) & (data.sqm <= 1.05 * sqm) & (data.elevator == elevator)]
+    if len(d) > 0:
+        p = d.price.mean()
+        comparative['by_sqm'] = roundk(p * .95)
+
+    d = data[(data.rooms == rooms) & (data.elevator == elevator)]
+    if len(d) > 0:
+        p = d.price.mean()
+        comparative['by_rooms'] = roundk(p * .95)
+
+    d = data[(data.state == state) & (data.elevator == elevator)]
+    if len(d) > 0:
+        p = d.price.mean()
+        comparative['by_state'] = roundk(p * .95)
+
+    return comparative
+
+
+def get_similar(data: pd.DataFrame, sqm: int, rooms: int, state: str, elevator: bool):
+    d = data[(data.sqm >= sqm * .75) & (data.sqm <= sqm * 1.25)]
+    d = d[(d.rooms >= rooms - 1) & (d.rooms <= rooms + 1)]
+
+    if len(d) > 10:
+        d = d[d.state == state]
+        if len(d) > 10:
+            d = d[d.elevator == elevator]
+
+    return d.to_dict('records')
+
+
+def get_result(data: pd.DataFrame, model: dict, sqm: int, rooms: int, state: str, elevator: bool):
+    prediction = get_prediction(model, sqm, rooms, state, elevator)
+    comparative = get_comparative(data, sqm, rooms, state, elevator)
+    similar = get_similar(data, sqm, rooms, state, elevator)
+
+    return dict(prediction=prediction,
+                comparative=comparative,
+                similar=similar)
+
+
+def roundk(x):
+    return 1000 * round(x/1000)
---
+++ frontend/src/index.js
@@ -0,0 +1,316 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AlgoliaPlaces from 'algolia-places-react';
+import {Container, Row, Col, Form, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
+
+import './pis.scss'
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {
+        neighbourhood: 'poblenou',
+        sqm: '',
+        rooms: '',
+        state: 'need_renovation',
+        elevator: 'true'
+      },
+      data: {
+        price: null,
+        upper_bound: null,
+        lower_bound: null
+      }
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleResult = this.handleResult.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      fields: { ...this.state.fields, [e.target.name]: e.target.value }
+    });
+  }
+
+  handleResult(data) {
+    this.setState({
+      ...this.state, data
+    })
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+
+    var url = new URL('http://localhost:8000/');
+    var params = this.state.fields;
+    url.search = new URLSearchParams(params).toString();
+
+    fetch(url, {method: 'GET'})
+    .then(res => res.json())
+    .then(data => this.handleResult(data));
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md={2}></Col>
+          <Col md={8} className="py-4">
+            <header>
+              <h1 className="h2">Location Intelligence for Real Estate</h1>
+            </header>
+          </Col>
+          <Col md={2}></Col>
+        </Row>
+        <Row>
+          <Col md={2}></Col>
+          <Col md={8}>
+            <Form onSubmit={this.submitForm}>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>Barri</Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    as="select"
+                    name="neighbourhood"
+                    value={this.state.fields.neighbourhood}
+                    onChange={this.handleInputChange}
+                  >
+                    <option value="poblenou">Poblenou</option>
+                    <option value="sarria">Sarrià</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>Adreça</Form.Label>
+                <Col sm={9}>
+                <AlgoliaPlaces
+                    placeholder="Carrer de..."
+                    options={{
+                        appId: 'pl0RMHINOPC7',
+                        apiKey: 'ee69abf250ec4f19adcc5e4e13b8ef62',
+                        countries: ['es'],
+                        aroundLatLngViaIP: false,
+                        aroundLatLng: '41.404507,2.162822',
+                        aroundRadius: 10*1000
+                    }}
+                />
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>Metres quadrats</Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    required
+                    name="sqm"
+                    type="number"
+                    placeholder="Superficie útil"
+                    value={this.state.fields.sqm}
+                    onChange={this.handleInputChange}
+                  ></Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Habitacions
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    required
+                    name="rooms"
+                    type="number"
+                    placeholder="Excluient menjador, cuina, banys, passadís, etc."
+                    value={this.state.fields.rooms}
+                    onChange={this.handleInputChange}
+                  ></Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Estat
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    as="select"
+                    name="state"
+                    value={this.state.fields.state}
+                    onChange={this.handleInputChange}
+                  >
+                    <option value="need_renovation">Necessita reforma</option>
+                    <option value="second_hand">Per entrar a viure</option>
+                    <option value="new">Per estrenar</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Ascensor
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control
+                    as="select"
+                    name="elevator"
+                    value={this.state.fields.elevator}
+                    onChange={this.handleInputChange}
+                  >
+                    <option value="true">Si</option>
+                    <option value="false">No</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Planta baixa
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control as="select">
+                    <option value="true">Si</option>
+                    <option value="false">No</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Ultima planta
+                </Form.Label>
+                <Col sm={9}>
+                  <Form.Control as="select">
+                    <option value="true">Si</option>
+                    <option value="false">No</option>
+                  </Form.Control>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Col>
+                    <Button type="submit" className="w-100">Calcular</Button>
+                </Col>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col md={2}></Col>
+        </Row>
+        <Row className="my-4">
+          <Col md={2}></Col>
+          <Col md={8} className="results">
+            <Results data={this.state.data}></Results>
+          </Col>
+          <Col md={2}></Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+class Results extends React.Component {
+  render() {
+    var prediction = null, comparative = null, similar = null;
+
+    if(this.props.data.prediction) {
+      prediction = (
+        <ListGroup>
+          <ListGroupItem variant="success" className="text-center">
+            <p>
+              Un bon preu per aquest pis és:
+            </p>
+            <p className="price">
+              &lt; {this.props.data.prediction.lower_bound}&euro;
+            </p>
+          </ListGroupItem>
+          <ListGroupItem variant="warning" className="text-center">
+            <p>
+              Un preu honest per aquest pis és:
+            </p>
+            <p className="price">
+              {this.props.data.prediction.cost}&euro; <br/>
+              <small>(o entre {this.props.data.prediction.lower_bound}&euro; i {this.props.data.prediction.upper_bound}&euro;)</small>
+            </p>
+          </ListGroupItem>
+          <ListGroupItem variant="danger" className="text-center">
+            <p>
+              Un preu excessiu per aquest pis és:
+            </p>
+            <p className="price">
+              &gt; {this.props.data.prediction.upper_bound}&euro;
+            </p>
+          </ListGroupItem>
+        </ListGroup>
+      );
+    }
+
+    if(this.props.data.comparative) {
+      comparative = (
+        <>
+          <header className="mt-4">
+            <h1 className="h3 text-center">
+              En aquest barri...
+            </h1>
+          </header>
+          <ListGroup>
+            <ListGroupItem className="text-center">
+              <p>
+                Pisos amb la mateixa superfície es venen per gairebé
+              </p>
+              <p className="price">
+                {this.props.data.comparative.by_sqm}&euro;
+              </p>
+            </ListGroupItem>
+            <ListGroupItem className="text-center">
+              <p>
+                Pisos amb el mateix nombre d'habitacions es venen per gairebé
+              </p>
+              <p className="price">
+                {this.props.data.comparative.by_rooms}&euro;
+              </p>
+            </ListGroupItem>
+            <ListGroupItem className="text-center">
+              <p>
+                Pisos en el mateix estat es venen per gairebé:
+              </p>
+              <p className="price">
+                {this.props.data.comparative.by_state}&euro;
+              </p>
+            </ListGroupItem>
+          </ListGroup>
+        </>
+      );
+    }
+
+    if(this.props.data.similar) {
+      const listItems = this.props.data.similar.map((item) =>
+        <ListGroupItem>
+          <header>
+            <h1 className="h5">
+              <a href={item.url}>{item.title}</a>
+            </h1>
+          </header>
+          <p>
+            {item.sqm} m<sup>2</sup>,&nbsp;
+            {item.rooms} habitacions,&nbsp;
+            <strong>{item.price}&euro;</strong>
+          </p>
+        </ListGroupItem>
+      )
+
+      similar = (
+        <>
+          <header className="mt-4">
+            <h1 className="h3 text-center">També et pot interessar...</h1>
+          </header>
+          <ListGroup>
+            {listItems}
+          </ListGroup>
+        </>
+      );
+    }
+
+    return <>
+      {prediction ? prediction : ''}
+      {comparative ? comparative : ''}
+      {similar ? similar : ''}
+    </>;
+  }
+}
+
+ReactDOM.render(<Page />, document.querySelector('#root'));
